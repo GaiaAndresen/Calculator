ce47896a57b53259ae968c8653ef09f39c260de6
GaiaAndresen <gaia.ulv@gmail.com>
2025-03-21 13:48:47 +0100

    Loading

--- history.go
+++ history.go
@@ -35,12 +35,21 @@ func loadResFromString(numStr string) float64 {
 	numStr = strings.ReplaceAll(numStr, " ", "")
 	numStr = strings.ReplaceAll(numStr, "(", "")
 	numStr = strings.ReplaceAll(numStr, ")", "")
+	recordsAmount := len(records)
 	if numStr == "" {
-		return load(len(records) - 1).result
+		if recordsAmount == 0 {
+			fmt.Println("No values in memory, insert 0")
+			return 0
+		}
+		return load(recordsAmount - 1).result
 	}
-	number, err := strconv.ParseFloat(numStr, 64)
+	number, err := strconv.Atoi(numStr)
 	if err != nil {
 		fmt.Println("Error, expected an index, but got:", numStr)
+		return 0
+	}
+	if number >= recordsAmount {
+		fmt.Printf("There are only %d elements in memory, (%d) is invalid", recordsAmount, number)
 		return 0
 	}
 	return load(int(number)).result
--- main.go
+++ main.go
@@ -4,43 +4,68 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
+	var loadedValue float64
+	loadedValueExists := false
 	fmt.Println("Calculator on")
 	for {
 		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
 		input := scanner.Text()
-		switch {
-		case input == "":
-		case input == "quit" || input == "q":
-			{
-				fmt.Println("Bye")
-				return
-			}
-		case input == "clear":
-			{
-				clearMemory()
-			}
-		case len(input) >= 4 && input[:4] == "load":
-			{
-				load := loadResFromString(input[4:])
-				fmt.Printf("Value %.2f loaded\n", load)
-			}
-		case len(input) >= 4 && input[:4] == "hist":
-			{
-				printHistory()
-			}
-		case (input[0] >= '0' && input[0] <= '9') || input[0] == '(' || input[0] == '-':
-			{
-				fmt.Println("calculating...")
-				result := getResult(input)
-				fmt.Println(result)
-			}
-		default:
-			{
-				fmt.Println("Please enter a valid command or expression")
+		if len(input) > 0 {
+			firstChar := input[0]
+			switch {
+			case input == "":
+			case input == "quit" || input == "q":
+				{
+					fmt.Println("Bye")
+					return
+				}
+			case input == "clear":
+				{
+					clearMemory()
+					loadedValueExists = false
+					fmt.Println("History cleared")
+				}
+			case len(input) >= 4 && input[:4] == "load":
+				{
+					loadedValue = loadResFromString(input[4:])
+					loadedValueExists = true
+					fmt.Printf("Value %.2f loaded\n", loadedValue)
+				}
+			case len(input) >= 3 && input[:3] == "del":
+				{
+					loadedValueExists = false
+				}
+			case len(input) >= 4 && input[:4] == "hist":
+				{
+					printHistory()
+				}
+			case (firstChar >= '0' && firstChar <= '9') || firstChar == '(' || firstChar == '-':
+				{
+					result := getResult(input)
+					fmt.Println(result)
+				}
+			case firstChar == '+' || firstChar == '-' || firstChar == '*' || firstChar == '/' || firstChar == '^':
+				{
+					if loadedValueExists {
+						input = strconv.FormatFloat(loadedValue, 'f', -1, 64) + input
+						result := getResult(input)
+						fmt.Println(result)
+					} else {
+						lastValue := strconv.FormatFloat(loadResFromString(""), 'f', -1, 64)
+						input = lastValue + input
+						result := getResult(input)
+						fmt.Println(result)
+					}
+				}
+			default:
+				{
+					fmt.Println("Please enter a valid command or expression")
+				}
 			}
 		}
 	}
